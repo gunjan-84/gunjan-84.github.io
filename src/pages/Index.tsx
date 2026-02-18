e848b402d4a2addd7d0cfe0195ebf8d0204cdede
gunjan-84 <solankigunjan02@gmail.com>
2026-02-18 12:17:54 +0530

    fixes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,7 +5,6 @@ import AboutSection from "@/components/AboutSection";
 import ExperienceSection from "@/components/ExperienceSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import SkillsSection from "@/components/SkillsSection";
-import AchievementsSection from "@/components/AchievementsSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
 
@@ -19,7 +18,6 @@ const Index = () => {
       <ExperienceSection />
       <ProjectsSection />
       <SkillsSection />
-      <AchievementsSection />
       <ContactSection />
       <Footer />
     </div>
